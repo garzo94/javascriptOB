2316ed7d9b6e7fbd62a2a2cf82a91be7db2b8cdb
garzo94 <alexgarzo25@gmail.com>
2022-06-11 09:07:02 -0600

    sesion 5

---
+++ NumerosEnJs.js
@@ -0,0 +1,11 @@
+let altura = 176
+let alturamt = 1.76
+let peso = 171.56
+
+let alturaRound = Math.round(alturamt)
+let pesoRound = Math.floor(peso)
+
+let maxJs = (Number.MAX_SAFE_INTEGER+1) === (Number.MAX_SAFE_INTEGER)
+console.log(maxJs)
+
+
