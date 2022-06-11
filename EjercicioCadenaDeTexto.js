8f7036c427129c6b908c19b3843cd2860627213f
garzo94 <alexgarzo25@gmail.com>
2022-06-11 08:32:06 -0600

    Ejercicio sesion 4

---
+++ EjercicioCadenaDeTexto.js
@@ -0,0 +1,11 @@
+let nombre = 'Alexander'
+let apellido = 'Garzo'
+let estudiante = nombre.concat(' ', apellido)
+let estudianteMayus = estu.toUpperCase()
+let estudianteMinus = estu.toLocaleLowerCase()
+let espaciosEstu = estu.length
+let firtsLetter = nombre.charAt(nombre.indexOf('A'))
+let lastLetter = apellido.charAt(apellido.indexOf('o'))
+let estuSinEsp = estudiante.trim()
+let isIn = estudiante.includes(nombre)
+
