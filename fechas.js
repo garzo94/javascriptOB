1409562a80fe4b8d6d18611916e744dff22c286b
garzo94 <alexgarzo25@gmail.com>
2022-06-12 12:10:31 -0600

    fechasJs

---
+++ fechas.js
@@ -0,0 +1,7 @@
+const hoy = new Date()
+const miNacimiento =  new Date(1994,1,13)
+const bool = hoy.getTime() === miNacimiento.getTime()
+const miDia =  new Date(1994,1,13).getDate()
+const miMes =miNacimiento.getMonth()
+const miYear = miNacimiento.getFullYear()
+
