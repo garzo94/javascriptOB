8f00a55c0955877a8ae06fb5ab97fc2e71243f47
garzo94 <alexgarzo25@gmail.com>
2022-06-19 15:55:57 -0600

    Ejercio modulos sesion 10

---
+++ modulosS10/controller.js
@@ -0,0 +1,7 @@
+export default function suma(a,b){
+  return a + b
+}
+
+export function multiplacion(a,b){
+    return a*b
+  }
---
+++ modulosS10/index.js
@@ -0,0 +1,6 @@
+import suma, {multiplacion} from './controller.js'
+import chalk from 'chalk'
+
+console.log(chalk.green(suma(1,2)))
+console.log(chalk.green(multiplacion(5,6)))
+
