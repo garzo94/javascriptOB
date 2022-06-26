a9fbda4839a628f040f74a5c8d83898cfbf0c794
garzo94 <alexgarzo25@gmail.com>
2022-06-26 15:00:53 -0600

    Ejercicio de eventos Sesion 14

---
+++ Eventos-S14/index.js
@@ -0,0 +1,10 @@
+// document.getElementById("btn").onclick = function() {myFunction()};
+
+// function myFunction() {
+//     alert('Click en el boton')
+//   }
+
+ $("button").click(function(){
+    console.log("Hola, estoy utilizando jQuery")
+ }) 
+
